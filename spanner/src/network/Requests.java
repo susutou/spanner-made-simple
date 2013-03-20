1b7f6fa9b45676f18170b2e3a80ce5ae5064bdc9
knightelvis <archerlaus@gmail.com>
2013-03-20 09:39:14 -0700

    fix an error

--- spanner/src/network/Requests.java
+++ spanner/src/network/Requests.java
@@ -15,12 +15,16 @@ public class Requests {
 
 	public static void sendRequestToPaxosLeader(String sender, int port,
 			String msg) throws Exception {
-		if (sender.contains("X"))
+		if (sender.contains("X")){
 			sendRequestTo(Agents.paxos_leaders[0], Agents.paxosPort, sender
 					+ "#" + msg);
-		else if (sender.contains("Y"))
+			System.out.println(sender+"#"+msg);
+		}
+		else if (sender.contains("Y")){
 			sendRequestTo(Agents.paxos_leaders[1], Agents.paxosPort, sender
 					+ "#" + msg);
+			System.out.println(sender+"#"+msg);	
+		}
 		else {
 			System.out.println("Server Name Error");
 		}
