b463292eb07981f415bde665fe8705d7f4e34cbd
knightelvis <archerlaus@gmail.com>
2013-03-20 07:37:06 -0700

    fix an error

--- spanner/src/network/Requests.java
+++ spanner/src/network/Requests.java
@@ -5,36 +5,45 @@ import java.net.Socket;
 
 public class Requests {
 
-	public static void sendRequestTo(String ip,  int port, String msg) throws Exception{
+	public static void sendRequestTo(String ip, int port, String msg)
+			throws Exception {
 		Socket socket = new Socket(ip, port);
 		DataOutputStream dos = new DataOutputStream(socket.getOutputStream());
 		dos.writeUTF(msg);
 		socket.close();
-    }
-	
-	public static void sendRequestToPaxosLeader(String sender, int port, String msg) throws Exception{
-		if(msg.contains("X"))
-			sendRequestTo(Agents.paxos_leaders[0],Agents.paxosPort,sender+"#"+msg);
-		else if(msg.contains("Y"))
-			sendRequestTo(Agents.paxos_leaders[1],Agents.paxosPort,sender+"#"+msg);
-		else{
-			sendRequestTo(Agents.paxos_leaders[0],Agents.paxosPort,sender+"#"+msg);
-			sendRequestTo(Agents.paxos_leaders[1],Agents.paxosPort,sender+"#"+msg);
-			}
 	}
-	
-	public static void sendRequestToPaxosLeader(String sender, String msg) throws Exception{
-		sendRequestToPaxosLeader(sender,Agents.paxosPort,msg);
-	}
-	
-	public static void sendRequestTo2PCCohort(String sender, String msg) throws Exception{
-		for(String hostname : Agents.tpc_corhort){
-			sendRequestTo(hostname,Agents.port,msg);
+
+	public static void sendRequestToPaxosLeader(String sender, int port,
+			String msg) throws Exception {
+		if (msg.contains("X"))
+			sendRequestTo(Agents.paxos_leaders[0], Agents.paxosPort, sender
+					+ "#" + msg);
+		else if (msg.contains("Y"))
+			sendRequestTo(Agents.paxos_leaders[1], Agents.paxosPort, sender
+					+ "#" + msg);
+		else {
+			sendRequestTo(Agents.paxos_leaders[0], Agents.paxosPort, sender
+					+ "#" + msg);
+			sendRequestTo(Agents.paxos_leaders[1], Agents.paxosPort, sender
+					+ "#" + msg);
 		}
 	}
-	
-	public static void sendRequestToServer(String serverName, String msg) throws Exception{
-		sendRequestTo(Agents.getIP(serverName),Agents.port,msg);
+
+	public static void sendRequestToPaxosLeader(String sender, String msg)
+			throws Exception {
+		sendRequestToPaxosLeader(sender, Agents.paxosPort, msg);
 	}
-	
+
+	public static void sendRequestTo2PCCohort(String sender, String msg)
+			throws Exception {
+		for (String hostname : Agents.tpc_corhort) {
+			sendRequestTo(hostname, Agents.port, msg);
+		}
+	}
+
+	public static void sendRequestToServer(String serverName, String msg)
+			throws Exception {
+		sendRequestTo(Agents.getIP(serverName), Agents.port, msg);
+	}
+
 }
