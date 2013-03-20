140fcbbb1ac5d65f372232949cd304edd9022d2a
knightelvis <archerlaus@gmail.com>
2013-03-20 11:36:56 -0700

    fix an error

--- spanner/src/twopc/TwoPCAgent.java
+++ spanner/src/twopc/TwoPCAgent.java
@@ -69,8 +69,9 @@ public class TwoPCAgent extends Agents implements Runnable {
 			logger.log(">>>--Send 2PC-Prepare MSG To Paxos Leader--->>>: " + newMsg+"\n");
 			System.out.println(">>>--Send 2PC-Prepare MSG To Paxos Leader--->>>: " + newMsg+"\n");
 			Requests.sendRequestToPaxosLeader(Utilities.getServerName(), newMsg);
-			
-			Requests.sendRequestTo2PC(Utilities.getServerName(), MessageHelper.removeHeaderFromMsg(msg));
+			logger.log("<<<--Get paxos_ready Msg--<<<: " + msg+"\n");
+			set2PCFailure();
+			//Requests.sendRequestTo2PC(Utilities.getServerName(), MessageHelper.removeHeaderFromMsg(msg));
 		
 		} catch (Exception e) {
 			e.printStackTrace();
@@ -82,7 +83,7 @@ public class TwoPCAgent extends Agents implements Runnable {
 	}
 	
 	public void set2PCFailure(){
-		tpcFailure = false;
+		tpcFailure = true;
 	}
 	
 	public void send2PCACK(String msg) {
@@ -104,6 +105,8 @@ public class TwoPCAgent extends Agents implements Runnable {
 
 	public boolean isAllPrepared(String opID, String ops) {
 		
+		return tpcFailure;
+		/*
 		if(paxosFailure) {
 			paxosFailure = false;
 			return false;
@@ -125,6 +128,7 @@ public class TwoPCAgent extends Agents implements Runnable {
 		}
 				
 		return false;
+		*/
 	}
 
 	public void commit2PC(String msg) {
